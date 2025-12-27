b973bb779adfa833d523966c23f8a95a39e67a1e
Tobias Horsamnn <tobias.horsmann@gmail.com>
2025-12-27 11:00:18 +0100

    moved mail to own subfolder

--- sharepoint2text/tests/test_extractions.py
+++ sharepoint2text/tests/test_extractions.py
@@ -3,8 +3,6 @@ import logging
 import unittest
 from unittest import TestCase
 
-from extractors.mail.eml_email_extractor import read_mail
-
 from sharepoint2text.extractors.data_types import (
     DocContent,
     DocxContent,
@@ -19,6 +17,7 @@ from sharepoint2text.extractors.data_types import (
 )
 from sharepoint2text.extractors.doc_extractor import read_doc
 from sharepoint2text.extractors.docx_extractor import read_docx
+from sharepoint2text.extractors.mail.eml_email_extractor import read_mail
 from sharepoint2text.extractors.pdf_extractor import read_pdf
 from sharepoint2text.extractors.plain_extractor import read_plain_text
 from sharepoint2text.extractors.ppt_extractor import read_ppt
